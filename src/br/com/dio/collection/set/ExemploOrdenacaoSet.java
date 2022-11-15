5831438c670891ef1d615f91a7d8f8ea75c53dd3
llpupp <llpupp@hotmail.com>
2022-11-15 13:53:37 -0300

    ExemploOrdenacaoSet

--- src/br/com/dio/collection/set/ExemploOrdenacaoSet.java
+++ src/br/com/dio/collection/set/ExemploOrdenacaoSet.java
@@ -115,5 +115,6 @@ class ComparatorNomeGeneroTempoEpisodio implements Comparator<Serie>{
         if (genero != 0) return genero;
 
         return Integer.compare(s1.getTempoEpisodio(), s2.getTempoEpisodio());
+
     }
 }
